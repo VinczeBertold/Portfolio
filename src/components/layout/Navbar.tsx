1ee94e9e8bc27b7d2ec43c2ede213f4057a59b43
BertoldVinczeIMC <21imc10072@fh-krems.ac.at>
2024-02-23 20:58:52 +0100

    style: Improve Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -6,46 +6,6 @@ export default async function Navbar() {
   return (
     <div className="navbar bg-base-300 p-2 text-base-content">
       <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
-          >
-            <li>
-              <a>Item 1</a>
-            </li>
-            <li>
-              <a>Parent</a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a>Item 3</a>
-            </li>
-          </ul>
-        </div>
         <a className="btn btn-ghost text-xl hidden md:flex text-center">
           Bertold Vincze
         </a>
@@ -71,7 +31,7 @@ export default async function Navbar() {
           theme={cookieJar.get("portfolio-theme")?.value || "dark"}
         />
         <a
-          className="btn btn-primary !rounded-sm w-32 hidden md:flex"
+          className="btn btn-primary !rounded-sm w-32"
           href="#contact"
         >
           Contact
